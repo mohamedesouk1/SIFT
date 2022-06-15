04102ace9c8c6949179c3b0ee2a8c108c62f786e
mohamedesouk1 <mdesouki2@gmail.com>
2022-06-15 08:40:39 +0200

    working without edge removal

--- code/student.py
+++ code/student.py
@@ -2,31 +2,59 @@ import numpy as np
 import cv2
 from skimage.filters import prewitt_h,prewitt_v
 from skimage.transform import rescale
+from skimage.feature import peak_local_max
 
 
 
-def get_interest_points(image, alpha=0.06):
+def get_interest_points(image, alpha=0.06, min_distance=20,thresh=0.1):
    
-    points=cv2.cornerHarris(image,4,5,alpha)
-    thresh=0.1*points.max()
+    R=cv2.cornerHarris(image,4,9,alpha)
+    thresh=0.1*R.max()
     x=[]
     y=[]
-    for i in range(0,points.shape[0]):
-        for j in range(0,points.shape[1]):
-            if points[i,j]>=thresh:
+    for i in range(0,R.shape[0]):
+        for j in range(0,R.shape[1]):
+            if R[i,j]>=thresh:
                 x.append(j)
                 y.append(i)
+    R[R < thresh*np.max(R)] = 0  # suppress any R value below threshold
+    points = peak_local_max(R, min_distance=min_distance)  # apply nonmaximum suppression
+
     print("number of harris points",len(x))
-    return x, y
+    #return points[:, 1], points[:, 0]
+    return x,y
 
 
+def ac_get_features(img, feature_width):
+    
+    gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    gray = cv2.normalize(gray, None, 0, 255, cv2.NORM_MINMAX).astype('uint8')
 
+    sift = cv2.SIFT.create(nfeatures = 0, nOctaveLayers = 4, contrastThreshold = 0.04, edgeThreshold = 0.1, sigma = 2)
+    kp1,dest = sift.detectAndCompute(gray,None)
+
+
+    #kpA = sift.detect(gray, None)
+    #kpA_computed, desA = sift.compute(gray, kpA) 
+    
+
+    drawAkp = cv2.drawKeypoints(gray, kp1, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    #cv2.imshow("A", drawAkp)
+
+    cv2.waitKey(0)
+    x=[]
+    y=[]
+    for i in kp1:
+        x.append(int(i.pt[0]))
+        y.append(int(i.pt[1]))
+    return dest,np.array(x),np.array(y)
+    
 def get_features(img, feature_width):
     
     def dog_octave(img,sigma,num_sigma=4):
 
         temp=img
-        k=2**(1/num_sigma)# so the last one has 2*sigma
+        k=2*(1/num_sigma)# so the last one has 2*sigma
         octave=[]
         dog_octave=[]
         cornersx=[]
@@ -36,8 +64,8 @@ def get_features(img, feature_width):
             if i !=0:
                 temp=cv2.GaussianBlur(img,(5,5),sigma*i*k)
             tempx,tempy=get_interest_points(temp)
-            cornersx.append(tempx)
-            cornersy.append(tempy)
+            cornersx.extend(tempx)
+            cornersy.extend(tempy)
 
             octave.append(temp)
             if i!=0:
@@ -54,8 +82,12 @@ def get_features(img, feature_width):
         cornersy=[]
         for i in range(1,num_scale+1):
             octave,temp_cornersx,temp_cornersy=dog_octave(temp,sigma,num_sigma)
-            cornersx.append(temp_cornersx)
-            cornersy.append(temp_cornersy)
+            if i==1:
+                cornersx.extend(temp_cornersx)
+                cornersy.extend(temp_cornersy)
+            else:
+                cornersx.extend(temp_cornersx*2*(i-1))
+                cornersy.extend(temp_cornersy*2*(i-1))
             dog_pyramid.append(octave)
             temp=rescale(temp,0.5)
         return dog_pyramid,cornersx,cornersy
@@ -71,10 +103,9 @@ def get_features(img, feature_width):
             for j in range(0,scale.shape[0]):# loop for different sigmas
                 print(scale[j].shape)
                 #tempy,tempx=get_interest_points(scale[j],feature_width)
-                print(np.array(cornersx[1]).shape)
-                for l,k in zip(cornersx[i][j],cornersy[i][j]):# loop for corner values detected by harris as the points have to be corners
+                for l,k in zip(cornersx,cornersy):# loop for corner values detected by harris as the points have to be corners
                 #for l in range(scale[j].shape[0]):
-                #   for k in range(scale[j].shape[1]):
+                 #   for k in range(scale[j].shape[1]):
                     l=int(l)
                     k=int(k)
                     if j==0:
@@ -82,7 +113,7 @@ def get_features(img, feature_width):
                     else:
                         neighbours=scale[j-1:j+1,k-1:k+1,l-1:l+1] # neighbour cube
                     if scale[0].shape[0]-int(feature_width/2)>=k>=int(feature_width/2) and scale[0].shape[1]-int(feature_width/2)>=l>=int(feature_width/2): # checks if it can be in the image given feature size
-                        if (scale[j,k,l]<=np.min(neighbours) or scale[j,k,l]>=np.max(neighbours)) and scale[j,k,l]>=0.08:# check if local minima or maxima also checks if above a certain threshold 
+                        if (scale[j,k,l]<=np.min(neighbours) or scale[j,k,l]>=np.max(neighbours)) and scale[j,k,l]>=0.03:# check if local minima or maxima also checks if above a certain threshold 
                             m=np.sqrt(((scale[0,k+1,l])-(scale[0,k-1,l]))**2+((scale[0,k,l+1])-(scale[0,k,l-1]))**2) # magnitude of keypoint
                             theta=2*np.pi+np.arctan2(((scale[0,k,l+1])-(scale[0,k,l-1])),((scale [0,k+1,l])-(scale[0,k-1,l])))# angle of keypoint
                             if i==0: 
@@ -90,13 +121,14 @@ def get_features(img, feature_width):
                                 y.append(k)
                                 max_matrix.append([l,k,i,m,theta])
                             else:               
-                                x.append(l*2*(i))
-                                y.append(k*2*(i))
-                                max_matrix.append([l*2*(i),k*2*(i),i,m,theta])
+                                x.append(l*2**(i))
+                                y.append(k*2**(i))
+                                max_matrix.append([l*2**(i),k*2**(i),i,m,theta])
         return np.array(max_matrix),x,y
     num_sigma=5
     num_scale=5
     dog_pyramid,cornersx,cornersy=dog_pyramid(img,num_sigma=num_sigma,num_scale=num_scale)
+
     max_matrix,x,y=check_max_neighbour(dog_pyramid,cornersx,cornersy,feature_width)
     features=np.zeros((max_matrix.shape[0],int(feature_width/4),int(feature_width/4),8))
     count=0
@@ -108,10 +140,9 @@ def get_features(img, feature_width):
         if i[2]==0:
             temp=img[k-int(feature_width/2):k+int(feature_width/2),l-int(feature_width/2):l+int(feature_width/2)]                      
         else:                                                                                                                          
-            tempk=int((k/(2*i[2])))                                                                                                    
-            templ=int((l/(2*i[2])))                                                                                                     
-            temp=rescale(img,(i[2]*0.5))[tempk-int(feature_width/2):tempk+int(feature_width/2),templ-int(feature_width/2):templ+int(feature_width/2)]          
-
+            tempk=int((k/(2**i[2])))                                                                                                    
+            templ=int((l/(2**i[2])))                                                                                                     
+            temp=rescale(img,(0.5**i[2]))[tempk-int(feature_width/2):tempk+int(feature_width/2),templ-int(feature_width/2):templ+int(feature_width/2)]          
 
         dy=prewitt_h(temp)
         dx=prewitt_v(temp)
@@ -119,25 +150,155 @@ def get_features(img, feature_width):
         
         angles=np.array(np.arctan2(dy,dx))
         angles +=np.pi
-        hist, bin_edges = np.histogram(angles, bins= 36, range=(0, 2*np.pi), weights=grad)
-        angles -= bin_edges[np.argmax(hist)]
-        angles[angles<0]=+2*np.pi
+        #hist, bin_edges = np.histogram(angles, bins= 36, range=(0, 2*np.pi), weights=grad)
+        #angles -= bin_edges[np.argmax(hist)]
+        #angles+=2*np.pi
+        angles_min=np.min(angles)
+        angles_max=np.max(angles)
 
         for j in range(0,int(feature_width/4)):
             for m in range(0,int(feature_width/4)):
                 temp_grad=grad[int((j+1)*feature_width/4-2*int(feature_width/8)):int((j+1)*feature_width/4),int((m+1)*feature_width/4-2*int(feature_width/8)):int((m+1)*feature_width/4)]
                 temp_angle=angles[int((j+1)*feature_width/4-2*int(feature_width/8)):int((j+1)*feature_width/4),int((m+1)*feature_width/4-2*int(feature_width/8)):int((m+1)*feature_width/4)]                
-                features[count,j,m]=np.histogram(temp_angle,bins=8,range=(0,2*np.pi),weights=temp_grad)[0]
+                features[count,j,m]=np.histogram(temp_angle,bins=8,range=(angles_min,angles_max),weights=temp_grad)[0]
         count+=1
-
-    features= features.reshape((max_matrix.shape[0], -1,))           
+    features= features.reshape((max_matrix.shape[0], -1,)) 
     dividend = np.linalg.norm(features, axis=1).reshape(-1, 1)
     # Rare cases where the gradients are all zeros in the window
     # Results in np.nan from division by zero.
     dividend[dividend == 0 ] = 1
     features = features / dividend
-    thresh = 0.25
-    features[ features >= thresh ] = thresh
+    print(features.shape)
+    return features,np.array(x),np.array(y)
+
+def vec_get_features(img, feature_width):
+    
+    def dog_octave(img,sigma,num_sigma=4):
+
+        temp=img
+        k=2*(1/num_sigma)# so the last one has 2*sigma
+        octave=[]
+        dog_octave=[]
+        cornersx=[]
+        cornersy=[]
+        for i in range(0,num_sigma):
+
+            if i !=0:
+                temp=cv2.GaussianBlur(img,(5,5),sigma*i*k)
+            tempx,tempy=get_interest_points(temp)
+            cornersx.extend(tempx)
+            cornersy.extend(tempy)
+
+            octave.append(temp)
+            if i!=0:
+                dog_octave.append(octave[i-1]-octave[i])
+
+
+        return dog_octave,cornersx,cornersy
+
+
+    def dog_pyramid(img,sigma=2,num_sigma=4,num_scale=4):
+        dog_pyramid=[]
+        temp=img
+        cornersx=[]
+        cornersy=[]
+        for i in range(1,num_scale+1):
+            octave,temp_cornersx,temp_cornersy=dog_octave(temp,sigma,num_sigma)
+            if i==1:
+                cornersx.extend(temp_cornersx)
+                cornersy.extend(temp_cornersy)
+            else:
+                cornersx.extend(temp_cornersx*2*(i-1))
+                cornersy.extend(temp_cornersy*2*(i-1))
+            dog_pyramid.append(octave)
+            temp=rescale(temp,0.5)
+        return dog_pyramid,cornersx,cornersy
+        
+
+    def check_max_neighbour(dog_pyramid,cornersx,cornersy,feature_width=16):
+        max_matrix=[]
+        x=[]
+        y=[]
+        for i in range(0,len(dog_pyramid)):# loop for different scales
+            scale=np.array(dog_pyramid[i])
+
+            for j in range(0,scale.shape[0]):# loop for different sigmas
+                print(scale[j].shape)
+                #tempy,tempx=get_interest_points(scale[j],feature_width)
+                for l,k in zip(cornersx,cornersy):# loop for corner values detected by harris as the points have to be corners
+                #for l in range(scale[j].shape[0]):
+                 #   for k in range(scale[j].shape[1]):
+                    l=int(l)
+                    k=int(k)
+                    if j==0:
+                        neighbours=scale[j:j+1,k-1:k+1,l-1:l+1] # neighbour cube
+                    else:
+                        neighbours=scale[j-1:j+1,k-1:k+1,l-1:l+1] # neighbour cube
+                    if scale[0].shape[0]-int(feature_width/2)>=k>=int(feature_width/2) and scale[0].shape[1]-int(feature_width/2)>=l>=int(feature_width/2): # checks if it can be in the image given feature size
+                        if (scale[j,k,l]<=np.min(neighbours) or scale[j,k,l]>=np.max(neighbours)) and scale[j,k,l]>=0.1:# check if local minima or maxima also checks if above a certain threshold 
+                            m=np.sqrt(((scale[0,k+1,l])-(scale[0,k-1,l]))**2+((scale[0,k,l+1])-(scale[0,k,l-1]))**2) # magnitude of keypoint
+                            theta=2*np.pi+np.arctan2(((scale[0,k,l+1])-(scale[0,k,l-1])),((scale [0,k+1,l])-(scale[0,k-1,l])))# angle of keypoint
+                            if i==0: 
+                                x.append(l)
+                                y.append(k)
+                                max_matrix.append([l,k,i,m,theta])
+                            else:               
+                                x.append(l*2**(i))
+                                y.append(k*2**(i))
+                                max_matrix.append([l*2**(i),k*2**(i),i,m,theta])
+        return np.array(max_matrix),x,y
+    num_sigma=5
+    num_scale=5
+    dog_pyramid,cornersx,cornersy=dog_pyramid(img,num_sigma=num_sigma,num_scale=num_scale)
+
+    max_matrix,x,y=check_max_neighbour(dog_pyramid,cornersx,cornersy,feature_width)
+    features=np.zeros((max_matrix.shape[0],int(feature_width/4)*int(feature_width/4),8))
+    count=0
+    print(max_matrix.shape)
+
+    all_dy=prewitt_h(img)
+    all_dx=prewitt_v(img)
+    all_grad=np.array(np.sqrt((all_dx**2)+(all_dy**2)))
+    
+    all_angles=np.array(np.arctan2(all_dy,all_dx))
+    all_angles +=np.pi
+    #hist, bin_edges = np.histogram(angles, bins= 36, range=(0, 2*np.pi), weights=grad)
+    #angles -= bin_edges[np.argmax(hist)]
+    #angles+=2*np.pi
+    angles_min=np.min(all_angles)
+    angles_max=np.max(all_angles)
+
+    x_all=[]
+    for i in max_matrix:        
+        k=int(i[1])
+        l=int(i[0])
+
+        if i[2]==0:
+            #temp=img[k-int(feature_width/2):k+int(feature_width/2),l-int(feature_width/2):l+int(feature_width/2)] 
+            grad=rescale(all_grad,(0.5**i[2]))[k-int(feature_width/2):k+int(feature_width/2),l-int(feature_width/2):l+int(feature_width/2)] 
+            angles=rescale(all_angles,(0.5**i[2]))[k-int(feature_width/2):k+int(feature_width/2),l-int(feature_width/2):l+int(feature_width/2)]                      
+        else:                                                                                                                          
+            tempk=int((k/(2**i[2])))                                                                                                    
+            templ=int((l/(2**i[2])))                                                                                                     
+            #temp=rescale(img,(0.5**i[2]))[tempk-int(feature_width/2):tempk+int(feature_width/2),templ-int(feature_width/2):templ+int(feature_width/2)]          
+
+            grad=rescale(all_grad,(0.5**i[2]))[tempk-int(feature_width/2):tempk+int(feature_width/2),templ-int(feature_width/2):templ+int(feature_width/2)]
+            angles=rescale(all_angles,(0.5**i[2]))[tempk-int(feature_width/2):tempk+int(feature_width/2),templ-int(feature_width/2):templ+int(feature_width/2)]
+
+        x=[]
+        features[count]=[np.histogram(angles[int((j+1)*feature_width/4-2*int(feature_width/8)):int((j+1)*feature_width/4),int((m+1)*feature_width/4-2*int(feature_width/8)):int((m+1)*feature_width/4)]
+            ,bins=8,range=(angles_min,angles_max),
+            weights=grad[int((j+1)*feature_width/4-2*int(feature_width/8)):int((j+1)*feature_width/4),int((m+1)*feature_width/4-2*int(feature_width/8)):int((m+1)*feature_width/4)])[0]
+                for j in range(0,int(feature_width/4)) 
+                    for m in range(0,int(feature_width/4))]
+        count+=1
+    features= features.reshape((max_matrix.shape[0], -1,)) 
+    dividend = np.linalg.norm(features, axis=1).reshape(-1, 1)
+    # Rare cases where the gradients are all zeros in the window
+    # Results in np.nan from division by zero.
+    dividend[dividend == 0 ] = 1
+    features = features / dividend
+    print(features.shape)
     return features,np.array(x),np.array(y)
 
 
